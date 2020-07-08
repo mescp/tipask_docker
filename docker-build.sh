332b51168b26e001860b59203a234e7c3ef26f75
liuth <liuth@web9s.com>
2020-07-08 15:02:15 +0800

    目录结构调整

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #获取源代码
-[ ! -d "src" ] && git clone https://github.com/sdfsky/tipask.git src
+[ ! -d "docker/tipask" ] && git clone https://github.com/sdfsky/tipask.git docker/tipask
 #拷贝卷结构
 [ ! -d "volumes" ] && mkdir volumes
 [ ! -d "volumes/storage" ] && cp -r src/storage ./volumes/storage
 [ ! -d "volumes/bootstrap" ] && cp -r src/bootstrap ./volumes/bootstrap
 touch volumes/.env
 #编译docker
-docker build --pull --rm -f "Dockerfile" -t tipask:latest "."
+docker build --pull --rm -f "docker/Dockerfile" -t tipask:latest "./docker"
 
