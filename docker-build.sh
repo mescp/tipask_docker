1b575fa33a6d25c486f4eec15e44ff4199819fde
liuth <liuth@web9s.com>
2020-07-08 21:03:18 +0800

    version:  php 7.2  & tipask 3.5

--- docker-build.sh
+++ docker-build.sh
@@ -4,8 +4,8 @@
 [ ! -d "docker/tipask" ] && git clone https://github.com/sdfsky/tipask.git docker/tipask
 #拷贝卷结构
 [ ! -d "volumes" ] && mkdir volumes
-[ ! -d "volumes/storage" ] && cp -r src/storage ./volumes/storage
-[ ! -d "volumes/bootstrap" ] && cp -r src/bootstrap ./volumes/bootstrap
+[ ! -d "volumes/storage" ] && cp -r docker/tipask/storage ./volumes/storage
+[ ! -d "volumes/bootstrap" ] && cp -r docker/tipask/bootstrap ./volumes/bootstrap
 touch volumes/.env
 #编译docker
 docker build --pull --rm -f "docker/Dockerfile" -t tipask:latest "./docker"
