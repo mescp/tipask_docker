0bcae202fb1089fa8378a04d2ec30179172578c7
liuth <liuth@web9s.com>
2020-07-09 10:01:46 +0800

    release deploy

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 #获取源代码
-[ ! -d "docker/tipask" ] && git clone https://github.com/sdfsky/tipask.git docker/tipask
+if [ ! -d "docker/tipask" ]; then
+    wget https://www.tipask.com/release/Tipask_v3.5.5_UTF8_20191017.zip -O docker/tipask.zip
+    unzip docker/tipask.zip -d docker/tipask
+fi
+
 #拷贝卷结构
 [ ! -d "volumes" ] && mkdir volumes
-[ ! -d "volumes/storage" ] && cp -r docker/tipask/storage ./volumes/storage
-[ ! -d "volumes/bootstrap" ] && cp -r docker/tipask/bootstrap ./volumes/bootstrap
-touch volumes/.env
+[ ! -d "volumes/tipask" ] && cp -r docker/tipask/v3.5.5 ./volumes/tipask
+
 #编译docker
 docker build --pull --rm -f "docker/Dockerfile" -t tipask:latest "./docker"
 
+if 
